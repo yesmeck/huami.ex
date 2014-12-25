705fb05091ae4a7e5591079358fceecf64f3deeb
Meck <yesmeck@gmail.com>
2014-12-25 18:47:30 +0800

    first work version

--- lib/huami.ex
+++ lib/huami.ex
@@ -1,5 +1,51 @@
 defmodule Huami do
-  def encode(key, password) do
 
+  @str1 "snow"
+  @str2 "kise"
+  @str3 "sunlovesnow1990090127xykab"
+
+  def encode(key, password) do
+    hmac_str1 = hmac(key, password)
+    hmac_str2 = hmac(@str1, hmac_str1)
+    hmac_str3 = hmac(@str2, hmac_str1)
+
+    rule = String.split(hmac_str3, "", trim: true)
+    source = String.split(hmac_str2, "", trim: true)
+
+    case_swapped_source = case_swap(rule, source)
+
+    ensure_first_char_is_str(case_swapped_source)
+  end
+
+  def hmac(key, password) do
+    :crypto.hmac(:md5, key, password)
+    |> Base.encode16
+    |> String.downcase
+  end
+
+  def case_swap(rule, source) do
+    _case_swarp(rule, source, [])
+  end
+
+  def _case_swarp([], [], swapped_source) do
+    swapped_source
+  end
+
+  def _case_swarp([rule_head | rule_tail], [source_head | source_tail], swapped_source) do
+    if String.contains?(@str3, rule_head) do
+      swapped_source = swapped_source ++ [ String.upcase(source_head) ]
+    else
+      swapped_source = swapped_source ++ [ source_head ]
+    end
+    _case_swarp(rule_tail, source_tail, swapped_source)
+  end
+
+  def ensure_first_char_is_str(source = [head | tail]) do
+    if Regex.match?(~r/[0-9]/, head) do
+      "K" <> (Enum.slice(source, 1, 15) |> Enum.join(""))
+    else
+      Enum.slice(source, 0, 16) |> Enum.join("")
+    end
   end
 end
+
--- test/huami_test.exs
+++ test/huami_test.exs
@@ -1,13 +1,32 @@
 defmodule HuamiTest do
   use ExUnit.Case
 
-  import Huami, only: [ encode: 2 ]
+  import Huami
 
-  test "key is one" do
-    assert encode('one', '123') == 'De46A920bB13D37c'
+  test "encode when key is one" do
+    assert encode("one", "123") == "De46A920bB13D37c"
   end
 
-  test "key is two" do
-    assert encode('two', '123') == 'K924748b267770BE'
+  test "encode when key is two" do
+    assert encode("two", "123") == "K924748b267770BE"
+  end
+
+  test "hmac" do
+    assert hmac("one", "123") == "7370c8d19b0cdee581ab85f92be29de9"
+  end
+
+  test "case_swap" do
+    rule = ["b", "c", "8", "2", "2", "f", "1", "7", "d", "e", "2", "c", "2", "1", "0", "c", "0", "c", "4", "c", "1", "8", "f", "2", "2", "8", "3", "d", "b", "e", "4", "7"]
+    source = ["d", "e", "4", "6", "a", "9", "2", "0", "b", "b", "1", "3", "d", "3", "7", "c", "1", "2", "5", "4", "5", "a", "b", "d", "7", "b", "a", "b", "3", "e", "4", "c"   ]
+    swapped_source = ["D", "e", "4", "6", "A", "9", "2", "0", "b", "B", "1", "3", "D", "3", "7", "c", "1", "2", "5", "4", "5", "a", "b", "D", "7", "b", "a", "b", "3", "E", "4", "C"]
+    assert case_swap(rule, source) == swapped_source
+  end
+
+  test "ensure_first_char_is_str when first char is string" do
+    assert ensure_first_char_is_str(["a", "1"]) == "a1"
+  end
+
+  test "ensure_first_char_is_str when first char is number" do
+    assert ensure_first_char_is_str(["1", "2"]) == "K2"
   end
 end
